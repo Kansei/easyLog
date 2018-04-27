aecdf750efb84f7ec38705e409ef2896f56dc87a
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2018-04-27 21:54:11 +0900

    exception: no log  in viewLogs

--- bin/viewLogs.rb
+++ bin/viewLogs.rb
@@ -2,6 +2,7 @@
 
 exit_word = ['quit', 'q', 'exit']
 except_word = [".","..",".gitkeep"]
+count = 0
 
 puts "========== Your logs ========="
 
@@ -9,15 +10,23 @@ logs_path = File.expand_path("../../mylogs",__FILE__)
 
 Dir.open(logs_path) do |dir|
   dir.each do |f|
+    count += 1
     if !except_word.include?(f)
       puts f
     end
+  end
+  
+  if count == 3
+    puts " Your log does not exist yet. "
+    puts "=============================="
+    exit
   end
 end
 puts "=============================="
 
 
 puts "choose a log to view!"
+print ">>"
 log_name = gets.chomp
 
 view_log = logs_path+"/"+log_name
