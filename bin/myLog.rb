fda22e8e01120b36ba4a3b81372d8a0fca1a38b9
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2018-04-27 19:24:55 +0900

    create: myLog viewLog

---
+++ bin/myLog.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+date = Time.now.strftime("%Y_%m_%d")
+logs_path = File.expand_path("../../mylogs",__FILE__)
+file_name = logs_path + "/" + date + "log.txt"
+
+exit_word = ['quit', 'q', 'exit']
+
+puts '======== start log ======='
+
+File.open(file_name, "a+") do |f|
+  while true do
+    log = gets.chomp
+
+    if exit_word.include?(log) then
+      puts '======== finish log ======='
+      break;
+    elsif log == ""
+    else
+      time = "["+Time.now.strftime("%Y:%m:%d:%H:%M")+"] "
+      f.puts time+log
+    end
+  end
+end
---
+++ bin/viewLogs.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+exit_word = ['quit', 'q', 'exit']
+except_word = [".",".."]
+
+puts "========== Your logs ========="
+
+logs_path = File.expand_path("../../mylogs",__FILE__)
+
+Dir.open(logs_path) do |dir|
+  dir.each do |f|
+    if !except_word.include?(f)
+      puts f
+    end
+  end
+end
+puts "=============================="
+
+
+puts "choose a log to view!"
+log_name = gets.chomp
+
+view_log = logs_path+"/"+log_name
+
+if File.exist?(view_log)
+  system('open '+ view_log)
+elsif exit_word.include?(log_name)
+  puts "see you!"
+else
+  puts "The log does not exist."
+  puts ""
+  system('viewLogs.rb')
+end
