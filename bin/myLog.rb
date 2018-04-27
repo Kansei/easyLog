92bbef187dfee3c270705d92ebfef09b7450d436
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2018-04-27 19:42:31 +0900

    fix:myLog

--- bin/myLog.rb
+++ bin/myLog.rb
@@ -2,7 +2,7 @@
 
 date = Time.now.strftime("%Y_%m_%d")
 logs_path = File.expand_path("../../mylogs",__FILE__)
-file_name = logs_path + "/" + date + "log.txt"
+file_name = logs_path + "/" + date + ".txt"
 
 exit_word = ['quit', 'q', 'exit']
 
