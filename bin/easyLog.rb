9cf46803d162ab92b26b6547979bb4153ec2d34e
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2018-08-04 04:55:55 +0900

    modify: easyLog to add new commands

--- bin/easyLog.rb
+++ bin/easyLog.rb
@@ -1,29 +1,50 @@
-load_path=File.expand_path("../",__FILE__)
+Dir[File.join(File.dirname(__FILE__), '/../commands/*.rb')].each { |file| require(file) }
 
-require "#{load_path}/viewLogs"
-require "#{load_path}/writeLog"
+# create (log) プロジェクトごとにログを作れる
+# delete (log) ログを消す
+# switch (log) ログを切り替えれる
+# list ログの一覧を確認できる
+# write ログを書く
+# view (log)ログの中身を見れる
 
 def check_argv
-  file = $0.split("/")[-1]
-  com = file.split(".")[0]
-  if ARGV.size != 1
-    STDERR.print "Usage: #{com} w/v (write or view)\n"
+  command = $0.split("/")[-1].split(".")[0]
+  if ARGV.size > 2
+    STDERR.print "Usage: #{command} w/v/l/s/d/c [log_name]\n"
     exit
   end
 end
 
-check_argv
-
-com = ARGV[0]
-
-case com
-when "w"
-  write_log
-when "v"
-  view_today_log
-when "l"
-  list_log
-else
-  STDERR.puts "command not found"
-  STDERR.puts "only 'w' or 'v', 'l'"
+def read_setfile
+  setfile_path = (File.join(File.dirname(__FILE__), '/../.set'))
+  setting_log = File.open(setfile_path, "r") do |file|
+    file.read
+  end
 end
+
+def main(argv)
+  sub_command = argv[0]
+  log_name = argv[1]
+  setting_log = read_setfile
+  case sub_command
+  when "w"
+    write_log(setting_log)
+  when "v"
+    log_name ||= setting_log
+    view_log(log_name)
+  when "s"
+    swittch_log(log_name)
+  when "d"
+    delete_log(log_name)
+  when "c"
+    create_log(log_name)
+  when "l"
+    list_log
+  else
+    STDERR.puts "command not found"
+    STDERR.puts "only w/v/l/s/d/c"
+  end
+end
+
+check_argv
+main(ARGV)
