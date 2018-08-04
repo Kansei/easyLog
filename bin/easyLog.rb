97839f0cdd1c6648284695eb87d8351e8c0b0809
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2018-08-04 10:47:30 +0900

    fix: bin/easyLog.rb

--- bin/easyLog.rb
+++ bin/easyLog.rb
@@ -10,15 +10,22 @@ Dir[File.join(File.dirname(__FILE__), '/../commands/*.rb')].each { |file| requir
 def check_argv
   command = $0.split("/")[-1].split(".")[0]
   if ARGV.size > 2
-    STDERR.print "Usage: #{command} w/v/l/s/d/c [log_name]\n"
+    STDERR.puts "Usage: #{command} w/v/l/s/d/c [log_name]\n"
     exit
   end
 end
 
 def read_setfile
-  setfile_path = (File.join(File.dirname(__FILE__), '/../.set'))
+  setfile_path = File.join(File.dirname(__FILE__), '/../.set')
   setting_log = File.open(setfile_path, "r") do |file|
-    file.read.chomp
+      file.read.chomp
+    end
+end
+
+def is_exist?(log_name)
+  if log_name.nil?
+    STDERR.puts "Please enter log name"
+    exit
   end
 end
 
@@ -33,10 +40,13 @@ def main(argv)
     log_name ||= setting_log
     view_log(log_name)
   when "s"
-    swittch_log(log_name)
+    is_exist?(log_name)
+    switch_log(log_name)
   when "d"
+    is_exist?(log_name)
     delete_log(log_name, setting_log)
   when "c"
+    is_exist?(log_name)
     create_log(log_name)
   when "l"
     list_log(setting_log)
