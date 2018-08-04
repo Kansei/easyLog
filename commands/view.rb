756f85fb9d710290477c4c0d6868e30d090f035f
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2018-08-04 10:46:56 +0900

    fix: open by vim

--- commands/view.rb
+++ commands/view.rb
@@ -2,7 +2,7 @@ def view_log(log_name)
   log_path = File.join(File.dirname(__FILE__), "/../logs/#{log_name}.txt")
 
   if File.exist?(log_path)
-    system('open '+ log_path)
+    system('vim '+ log_path)
   else
     STDERR.puts "#{log_name} does not exist."
   end
